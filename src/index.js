cb97914f3d72d5f3d09e96c249ef97a13c523541
Oscar Gonzalez <oscarrrr12@gmail.com>
2024-10-24 12:35:07 +0200

    build: add MongoDB connection to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const router = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -14,6 +15,16 @@ app.use(express.json());
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
+
+mongoose
+    .connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`)
+    .then(() => {
+        console.log('Successfully connect to MongoDB');
+    })
+    .catch(err => {
+        console.log('Connection error:', err);
+        process.exit();
+    });
 
 app.listen(PORT, () => {
     console.log(`Server listening at PORT: ${PORT}`);
