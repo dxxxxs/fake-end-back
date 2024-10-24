cf5bc4d4d9e556bc60a416730b783152a9b198f8
Oscar Gonzalez <oscarrrr12@gmail.com>
2024-10-24 18:22:09 +0200

    style: remove console.log and unused require

--- src/use-cases/delete-user.js
+++ src/use-cases/delete-user.js
@@ -1,12 +1,10 @@
 const userRepository = require('../repositories/user.repository');
-const bcrypt = require('bcrypt');
 
 class DeleteUser {
     async execute(_id) {
 
         const user = await userRepository.getUserById(_id);
 
-        console.log(_id);
         if (!user) {
             throw new Error('User does not exists');
         }
