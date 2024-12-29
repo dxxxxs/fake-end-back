c34ab1d93eaad45cf9697774eeb3d91bb70305d9
Oscar Gonzalez <oscarrrr12@gmail.com>
2024-12-30 00:42:22 +0100

    feat(auth.routes.js): apply login limiter middleware to login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const loginLimiter = require('../middlewares/loginLimiter');
 const controller = require('../controllers/auth.controller');
+const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/register', controller.register);
-router.post('/login', controller.login);
+router.post('/login', loginLimiter, controller.login);
 
 module.exports = router;
